a861f6378418de9fc2d3b8f31d34c79a7d0183ae
wdev00-lgtm <wdev67283@gmail.com>
2026-02-18 18:13:38 +0530

    Update script.js
    java script added now

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const cartModal = document.getElementById("cartModal");
+const cartIcon = document.querySelector(".nav-icons span:last-child");
+
+cartIcon.addEventListener("click", () => {
+    cartModal.style.display = "block";
+});
+
+function closeCart() {
+    cartModal.style.display = "none";
+}
 
