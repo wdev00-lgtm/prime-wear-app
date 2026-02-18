3c16aa0668708378ebf98156c15c2faa3c3b8282
wdev00-lgtm <wdev67283@gmail.com>
2026-02-18 18:30:31 +0530

    Update app.js
    finall app js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
+function addToCart(name, price) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push({ name, price });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert("Added to cart!");
+}
+
+function loadCart() {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let cartContainer = document.getElementById("cart-items");
+    let total = 0;
+
+    cartContainer.innerHTML = "";
+
+    cart.forEach(item => {
+        total += parseInt(item.price.replace(/[^\d]/g, ""));
+        cartContainer.innerHTML += `
+            <p>${item.name} - ${item.price}</p>
+        `;
+    });
+
+    document.getElementById("total").innerText = "₹" + total;
+}
+
+function clearCart() {
+    localStorage.removeItem("cart");
+    location.reload();
+}
 
